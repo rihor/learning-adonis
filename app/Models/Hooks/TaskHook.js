3f98dbda6c8e2ab8139ca5c0e4af5b73eb85098f
Pedro Pinho <rihor1000@gmail.com>
2019-12-24 13:25:15 -0300

    hook no Task

---
+++ app/Models/Hooks/TaskHook.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const Mail = use('Mail')
+const Helpers = use('Helpers')
+
+const TaskHook = (exports = module.exports = {})
+
+TaskHook.sendNewTaskMail = async taskInstance => {
+  if (!taskInstance.user_id && !taskInstance.dirty.user_id) return
+
+  // busca pelo usuário relacionado a essa Task
+  const { title } = taskInstance
+  const { email, username } = await taskInstance.user().fetch()
+  const file = await taskInstance.file().fetch()
+
+  await Mail.send(
+    ['emails.new_task'],
+    { username, title, hasAttachment: !!file },
+    message => {
+      message
+        .to(email)
+        .from('pedro@gmail.com', 'Pedro AdonisJS')
+        .subject('Nova tarefa para você')
+
+      if (file) {
+        message.attach(Helpers.tmpPath(`uploads/${file.file}`), {
+          filename: file.name
+        })
+      }
+    }
+  )
+}
