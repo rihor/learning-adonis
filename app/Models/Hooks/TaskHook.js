1df5072cec9a8a35e4e76de7d6de5f1c85154b23
Pedro Pinho <rihor1000@gmail.com>
2019-12-25 11:48:20 -0300

    Fila no Redis para o job de envio de email

--- app/Models/Hooks/TaskHook.js
+++ app/Models/Hooks/TaskHook.js
@@ -1,7 +1,7 @@
 'use strict'
 
-const Mail = use('Mail')
-const Helpers = use('Helpers')
+const Kue = use('Kue')
+const Job = use('App/Jobs/NewTaskMail')
 
 const TaskHook = (exports = module.exports = {})
 
@@ -13,20 +13,5 @@ TaskHook.sendNewTaskMail = async taskInstance => {
   const { email, username } = await taskInstance.user().fetch()
   const file = await taskInstance.file().fetch()
 
-  await Mail.send(
-    ['emails.new_task'],
-    { username, title, hasAttachment: !!file },
-    message => {
-      message
-        .to(email)
-        .from('pedro@gmail.com', 'Pedro AdonisJS')
-        .subject('Nova tarefa para você')
-
-      if (file) {
-        message.attach(Helpers.tmpPath(`uploads/${file.file}`), {
-          filename: file.name
-        })
-      }
-    }
-  )
+  Kue.dispatch(Job.key, { email, username, file, title }, { attempts: 3 })
 }
